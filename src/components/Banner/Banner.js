166f35def78459de40a4eeb270eee6a57ccb81de
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-05-29 03:35:47 +0600

    add img and text and button with css style

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,7 +1,10 @@
 import React from "react";
+import "./Banner.css";
 import slider1 from "../../images/slider/slider-1.webp";
 import slider2 from "../../images/slider/slider-2.webp";
 import slider3 from "../../images/slider/slider-3.webp";
+import rightImg1 from "../../images/Right-side-banner/blog-thumb-2.jpg";
+import rightImg2 from "../../images/Right-side-banner/blog-thumb-3.jpg";
 
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -16,27 +19,69 @@ import { Navigation } from "swiper";
 const Banner = () => {
   return (
     <div>
-      <div>
-        <>
+      <div className="banner-childParent-grid-div">
+        <div>
           <Swiper
+            // style={{ width: "50%" }}
             rewind={true}
             navigation={true}
             modules={[Navigation]}
             className="mySwiper"
           >
             <SwiperSlide>
-              <img src={slider1} alt="" />
+              <img className="slider-img" src={slider1} alt="" />
+              <div className="img-text-div">
+                <h3>The Best Quality Products Guaranteed!</h3>
+                <p>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Totam quibusdam maiores quasi architecto id obcaecati sapiente
+                  perferendis beatae iure in? Lorem ipsum, dolor sit amet
+                  consectetur adipisicing elit. Totam quibusdam maiores quasi
+                  architecto id obcaecati sapiente perferendis beatae iure in?
+                </p>
+                <button>Shop Now</button>
+              </div>
             </SwiperSlide>
             <SwiperSlide>
-              <img src={slider2} alt="" />
+              <img className="slider-img" src={slider2} alt="" />
+              <div className="img-text-div">
+                <h3>The Best Quality Products Guaranteed!</h3>
+                <p>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Totam quibusdam maiores quasi architecto id obcaecati sapiente
+                  perferendis beatae iure in? Lorem ipsum, dolor sit amet
+                  consectetur adipisicing elit. Totam quibusdam maiores quasi
+                  architecto id obcaecati sapiente perferendis beatae iure in?
+                </p>
+                <button>Shop Now</button>
+              </div>
             </SwiperSlide>
             <SwiperSlide>
-              <img src={slider3} alt="" />
+              <img className="slider-img" src={slider3} alt="" />
+              <div className="img-text-div">
+                <h3>The Best Quality Products Guaranteed!</h3>
+                <p>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Totam quibusdam maiores quasi architecto id obcaecati sapiente
+                  perferendis beatae iure in? Lorem ipsum, dolor sit amet
+                  consectetur adipisicing elit. Totam quibusdam maiores quasi
+                  architecto id obcaecati sapiente perferendis beatae iure in?
+                </p>
+                <button>Shop Now</button>
+              </div>
             </SwiperSlide>
           </Swiper>
-        </>
+        </div>
+        <div className="right-banner-div">
+          <h3>Latest Super Discount Active Coupon Code</h3>
+          <div>
+            <img style={{ width: "100px" }} src={rightImg1} alt="" />
+          </div>
+          <div>
+            <img style={{ width: "100px" }} src={rightImg2} alt="" />
+          </div>
+        </div>
       </div>
-      <div></div>
     </div>
   );
 };
